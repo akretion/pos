e1ff810c49f26055e2a8625ac518fbb304806a91
hparfr <raph@hpar.fr>
2018-03-23 11:14:52 +0100

    update strings in config

--- pos_order_to_sale_order/models/pos_config.py
+++ pos_order_to_sale_order/models/pos_config.py
@@ -11,21 +11,21 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     iface_allow_pos_order = fields.Boolean(
-        string='PoS Orders', default=True,
+        string='Allow PoS Orders', default=True,
         help="Allow PoS Orders")
 
     iface_allow_draft_order = fields.Boolean(
-        string='Quotations', default=True,
+        string='Allow Quotations', default=True,
         help="Allow creation of quotations from the PoS.",
         old_name="iface_create_draft_sale_order")
 
     iface_allow_confirmed_order = fields.Boolean(
-        string='Sale Orders', default=True,
+        string='Allow Sale Orders', default=True,
         help="Allow creation of sale orders from the PoS.",
         old_name="iface_create_confirmed_sale_order")
 
     iface_allow_delivered_order = fields.Boolean(
-        string='Delivered Sale Orders', default=True,
+        string='Allow Delivered Sale Orders', default=True,
         help="Allow creation of delivered sale orders."
         " The according picking will be marked as delivered. Only invoices"
         " process will be possible.",
