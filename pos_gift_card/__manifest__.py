fbc575009256641b2c62c2ce2a3a97a1c09a6c4a
hparfr <raphael.reverdy@akretion.com>
2025-09-08 11:56:37 +0200

    pos_gift_card: remove useless file

--- pos_gift_card/__manifest__.py
+++ pos_gift_card/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "pos_gift_card",
     "summary": "pos_gift_card",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "category": "Point of Sale",
     "website": "https://github.com/OCA/pos",
     "author": "Akretion, Odoo Community Association (OCA)",
--- pos_gift_card/models/__init__.py
+++ pos_gift_card/models/__init__.py
@@ -1,4 +1,3 @@
-from . import pos_config
 from . import pos_order
 from . import pos_payment
 from . import gift_card_line
